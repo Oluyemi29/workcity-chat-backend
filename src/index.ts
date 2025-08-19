e212e08a97139159d294bef3a9ac75d4bfbdcf45
Oluyemi29 <adedokunoluyemi29@gmail.com>
2025-08-19 12:44:13 +0100

    update indexjs page

--- src/index.ts
+++ src/index.ts
@@ -8,21 +8,19 @@ import messageRouter from "./route/messageRoute.js";
 import { app, server } from "./lib/server.js";
 import authRouter from "./route/auth.js";
 import ConnectDB from "./connect/ConnectDb.js";
-// import job from "./cron.js";
 
 dotenv.config();
 ConnectDB();
 app.use(cookieParser());
 app.use(
   cors({
-    origin: [process.env.FrontendUrl as string],
+    origin: process.env.FrontendUrl as string,
     credentials: true,
   })
 );
 app.use(morgan("dev"));
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb" }));
-// job.start();
 app.use("/api", authRouter);
 app.use("/api", conversationRouter);
 app.use("/api", messageRouter);
